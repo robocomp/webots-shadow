1a66de4a497ed34a6a03b552a839c981726fd52e
SergioTrac <sergiotraceslava@gmail.com>
2023-12-22 13:08:58 +0100

    Sandra now walks between rooms.

---
+++ controllers/crowd_controller_four_rooms/build/release/crowd_controller_four_rooms.d
@@ -0,0 +1,10 @@
+build/release/crowd_controller_four_rooms.o: \
+ crowd_controller_four_rooms.cpp \
+ /usr/local/webots/include/controller/cpp/webots/Supervisor.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Device.hpp \
+ /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
+ /usr/local/webots/include/controller/cpp/webots/Node.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Field.hpp \
+ /usr/local/webots/include/controller/cpp/webots/../../c/webots/contact_point.h \
+ /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
+ CrowdManager_four_rooms.cpp
