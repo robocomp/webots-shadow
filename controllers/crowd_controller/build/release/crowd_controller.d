f705ac5c5bc766fe1aa6bba5f3c758ef7ac713d0
SergioTrac <sergiotraceslava@gmail.com>
2024-01-08 12:04:24 +0100

    CrowdManager removed. CrowdAgent and crowd_controller separation.

--- controllers/crowd_controller/build/release/crowd_controller.d
+++ controllers/crowd_controller/build/release/crowd_controller.d
@@ -1,9 +1,8 @@
-build/release/crowd_controller.o: crowd_controller.cpp \
+build/release/crowd_controller.o: crowd_controller.cpp CrowdAgent.h \
  /usr/local/webots/include/controller/cpp/webots/Supervisor.hpp \
  /usr/local/webots/include/controller/cpp/webots/Device.hpp \
  /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
  /usr/local/webots/include/controller/cpp/webots/Node.hpp \
  /usr/local/webots/include/controller/cpp/webots/Field.hpp \
  /usr/local/webots/include/controller/cpp/webots/../../c/webots/contact_point.h \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
- CrowdManager.cpp
+ /usr/local/webots/include/controller/cpp/webots/Robot.hpp
